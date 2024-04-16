21e4fae0ead0e50675d61a5687c97504f56ca231
Pavel Shemetov <kkollop123@gmail.com>
2024-04-16 13:39:04 +0500

    таск 3

--- index.js
+++ index.js
@@ -20,3 +20,29 @@ addbutton.addEventListener('click', (e) => {
     const form = document.getElementsByTagName('form')[0]
     form.insertBefore(clone, document.getElementsByClassName('123')[0])
 })
+
+
+const modal = document.querySelector('dialog')
+const modalBox = document.getElementById('modal-box')
+const showModalBtn = document.getElementById('show-modal-btn')
+const closeModalBtn = document.getElementById('close-modal-btn')
+
+let isModalOpen = false
+
+showModalBtn.addEventListener('click', (e) => {
+    e.preventDefault();
+    modal.showModal()
+    isModalOpen = true
+    e.stopPropagation()
+})
+
+closeModalBtn.addEventListener('click', () => {
+    modal.close()
+    isModalOpen = false
+})
+
+document.addEventListener('click', (e) => {
+    if (isModalOpen && !modalBox.contains(e.target)) {
+        modal.close()
+    }
+})
